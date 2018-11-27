97442397c76a74151093128afa06e46bf76a1cb1
Francois Gindraud <francois.gindraud@univ-lyon1.fr>
2018-11-27 16:09:57 +0100

    Framework for G computation in the kernel histogram case

--- src/computations.h
+++ src/computations.h
@@ -103,8 +103,8 @@ inline int64_t tmax_for_region (const ProcessesData<Point> & processes, RegionId
  * This is more efficient because the upper bound of the k-th interval is the lower bound of the (k+1)-th.
  * Thus we compute the bounds only once.
  */
-inline std::vector<int64_t> compute_b_ml_histogram_counts_for_all_k (const SortedVec<Point> & m_process,
-                                                                     const SortedVec<Point> & l_process,
+inline std::vector<int64_t> compute_b_ml_histogram_counts_for_all_k (const SortedVec<Point> & m_points,
+                                                                     const SortedVec<Point> & l_points,
                                                                      const HistogramBase & base) {
 	// Accumulator for sum_{x_l} count ({x_m, (x_m - x_l) in ] k*delta, (k+1)*delta ]})
 	std::vector<int64_t> counts (base.base_size, 0);
@@ -112,14 +112,14 @@ inline std::vector<int64_t> compute_b_ml_histogram_counts_for_all_k (const Sorte
 	// sib[k] = index of first x_m with x_m - x_l > k*delta
 	std::vector<int32_t> sliding_interval_bounds (base.base_size + 1, 0);
 
-	const auto n_m = m_process.size ();
-	for (const Point x_l : l_process) {
+	const auto n_m = m_points.size ();
+	for (const Point x_l : l_points) {
 		// Compute indexes of all k interval boundaries, shifted from the current x_l.
 		// This can be done by searching m points starting at the previous positions (x_l increased).
 		for (int32_t k = 0; k < base.base_size + 1; ++k) {
 			const int32_t shift = k * base.delta;
 			int32_t i = sliding_interval_bounds[k];
-			while (i < n_m && !(m_process[i] - x_l > shift)) {
+			while (i < n_m && !(m_points[i] - x_l > shift)) {
 				i += 1;
 			}
 			sliding_interval_bounds[k] = i;
@@ -143,8 +143,8 @@ inline std::vector<int64_t> compute_b_ml_histogram_counts_for_all_k (const Sorte
  * The strategy is to compute the integral by splitting R in the constant parts of N_l(..) * N_l2(..).
  * Thus we loop over all points of changes of this product.
  */
-inline int64_t compute_g_ll2kk2_histogram_integral (const SortedVec<Point> & l_process,
-                                                    const SortedVec<Point> & l2_process,
+inline int64_t compute_g_ll2kk2_histogram_integral (const SortedVec<Point> & l_points,
+                                                    const SortedVec<Point> & l2_points,
                                                     HistogramBase::Interval interval,
                                                     HistogramBase::Interval interval2) {
 	// TODO replace Point by int64_t to avoid overflows ?
@@ -190,8 +190,8 @@ inline int64_t compute_g_ll2kk2_histogram_integral (const SortedVec<Point> & l_p
 
 	int64_t accumulated_area = 0;
 	Point current_x = 0;
-	SlidingInterval si1 (l_process, interval);
-	SlidingInterval si2 (l2_process, interval2);
+	SlidingInterval si1 (l_points, interval);
+	SlidingInterval si2 (l2_points, interval2);
 
 	while (true) {
 		const Point next_x = std::min ({si1.next_x_entering, si1.next_x_exiting, si2.next_x_entering, si2.next_x_exiting});
@@ -322,7 +322,7 @@ inline auto to_shape (IntervalKernel kernel) {
 	return shape::IntervalIndicator::with_width (kernel.width);
 }
 
-inline double compute_b_mlk_histogram (const SortedVec<Point> & m_process, const SortedVec<Point> & l_process,
+inline double compute_b_mlk_histogram (const SortedVec<Point> & m_points, const SortedVec<Point> & l_points,
                                        HistogramBase::Interval base_interval, IntervalKernel m_kernel,
                                        IntervalKernel l_kernel) {
 	// Base shapes
@@ -338,9 +338,17 @@ inline double compute_b_mlk_histogram (const SortedVec<Point> & m_process, const
 
 	// b_mlk = sum_{x_m in N_m, x_l in N_l} convolution(w_m,w_l,phi_k)(x_m - x_l)
 	// Split the sum into separate sums for each component.
-	return compute_sum_of_point_differences (m_process, l_process, a) +
-	       compute_sum_of_point_differences (m_process, l_process, b) +
-	       compute_sum_of_point_differences (m_process, l_process, c);
+	return compute_sum_of_point_differences (m_points, l_points, a) +
+	       compute_sum_of_point_differences (m_points, l_points, b) +
+	       compute_sum_of_point_differences (m_points, l_points, c);
+}
+
+inline double compute_g_ll2kk2_histogram_integral (const SortedVec<Point> & l_points,
+                                                   const SortedVec<Point> & l2_points, HistogramBase::Interval interval,
+                                                   HistogramBase::Interval interval2, IntervalKernel kernel,
+                                                   IntervalKernel kernel2) {
+	// TODO
+	return 0;
 }
 
 inline MatrixB compute_b (const ProcessesData<Point> & processes, RegionId region, HistogramBase base,
@@ -368,4 +376,71 @@ inline MatrixB compute_b (const ProcessesData<Point> & processes, RegionId regio
 		}
 	}
 	return b;
+}
+
+inline MatrixG compute_g (const ProcessesData<Point> & processes, RegionId region, HistogramBase base,
+                          span<const IntervalKernel> kernels) {
+	assert (int32_t (kernels.size ()) == processes.nb_processes ());
+	const auto nb_processes = processes.nb_processes ();
+	const auto base_size = base.base_size;
+	const auto sqrt_delta = std::sqrt (double(base.delta));
+	MatrixG g (nb_processes, base_size);
+
+	g.set_tmax (tmax_for_region (processes, region));
+
+	for (ProcessId l{0}; l.value < nb_processes; ++l.value) {
+		/* g_lk = sum_{x_m} integral convolution(w_l,phi_k) (x - x_m) dx.
+		 * g_lk = sum_{x_m} (integral w_l) (integral phi_k) = sum_{x_m} eta_l sqrt(delta) = |N_m| eta_l sqrt(delta).
+		 */
+		const auto g_lk = processes.data (l, region).size () * double(kernels[l.value].width) * sqrt_delta;
+		for (FunctionBaseId k{0}; k.value < base_size; ++k.value) {
+			g.set_g (l, k, g_lk);
+		}
+	}
+
+	auto G_value = [&](ProcessId l, ProcessId l2, FunctionBaseId k, FunctionBaseId k2) {
+		return compute_g_ll2kk2_histogram_integral (processes.data (l, region), processes.data (l2, region),
+		                                            base.interval (k), base.interval (k2), kernels[l.value],
+		                                            kernels[l2.value]);
+	};
+	/* G symmetric, only compute for (l2,k2) >= (l,k) (lexicographically).
+	 *
+	 * G_{l,l2,k,k2} = integral_x sum_{x_l,x_l2} conv(w_l,phi_k) (x - x_l) conv(w_l2,phi_k2) (x - x_l2) dx.
+	 * By change of variable x -> x + c*delta: G_{l,l2,k,k2} = G_{l,l2,k+c,k2+c}.
+	 * Thus for each block of G for (l,l2), we only need to compute 2K+1 values of G.
+	 * We compute the values on two borders, and copy the values for all inner cells.
+	 */
+	for (ProcessId l{0}; l.value < nb_processes; ++l.value) {
+		// Case l2 == l: compute for k2 >= k:
+		for (FunctionBaseId k{0}; k.value < base_size; ++k.value) {
+			// Compute G_{l,l,0,k} and copy to G_{l,l,c,k+c} for k in [0,K[.
+			const auto v = G_value (l, l, FunctionBaseId{0}, k);
+			for (int32_t c = 0; k.value + c < base_size; ++c) {
+				g.set_G (l, l, FunctionBaseId{c}, FunctionBaseId{k.value + c}, v);
+			}
+		}
+		// Case l2 > l: compute for all (k,k2):
+		for (ProcessId l2{l.value}; l2.value < nb_processes; ++l2.value) {
+			// Compute G_{l,l2,0,0} and copy to G_{l,l2,c,c}.
+			{
+				const auto v = G_value (l, l2, FunctionBaseId{0}, FunctionBaseId{0});
+				for (int32_t c = 0; c < base_size; ++c) {
+					g.set_G (l, l2, FunctionBaseId{c}, FunctionBaseId{c}, v);
+				}
+			}
+			/* for k in [1,K[:
+			 * Compute G_{l,l2,0,k} and copy to G_{l,l2,c,k+c}.
+			 * Compute G_{l,l2,k,0} and copy to G_{l,l2,k+c,c}.
+			 */
+			for (FunctionBaseId k{1}; k.value < base_size; ++k.value) {
+				const auto v_0k = G_value (l, l2, FunctionBaseId{0}, k);
+				const auto v_k0 = G_value (l, l2, k, FunctionBaseId{0});
+				for (int32_t c = 0; k.value + c < base_size; ++c) {
+					g.set_G (l, l2, FunctionBaseId{c}, FunctionBaseId{k.value + c}, v_0k);
+					g.set_G (l, l2, FunctionBaseId{k.value + c}, FunctionBaseId{c}, v_k0);
+				}
+			}
+		}
+	}
+	return g;
 }
