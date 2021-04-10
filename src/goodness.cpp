7332062890d1ea459b84654055df2278bffe484f
François Gindraud <francois.gindraud@gmail.com>
2021-04-10 16:14:40 +0200

    Goodness: parsing of estimated a

--- src/goodness.cpp
+++ src/goodness.cpp
@@ -6,6 +6,57 @@
 #include "goodness.h"
 #include "input.h"
 
+Matrix_M_MK1 read_estimated_a_from(string_view filename, std::size_t nb_processes, std::size_t base_size) {
+    try {
+        auto estimated_a = Matrix_M_MK1(nb_processes, base_size);
+        const auto start = instant();
+        auto file = open_file(filename, "r");
+        auto reader = LineByLineReader(file.get());
+        auto get_next_non_comment_line = [&reader]() -> string_view {
+            while(reader.read_next_line()) {
+                string_view line = trim_ws(reader.current_line());
+                if(!starts_with('#', line)) {
+                    return line;
+                }
+            }
+            throw std::runtime_error("Expected line with values");
+        };
+        try {
+            // Expected content is known, "demand driven" parser.
+            {
+                auto values_text = split('\t', get_next_non_comment_line());
+                if(values_text.size() != nb_processes) {
+                    throw std::runtime_error(
+                        fmt::format("a_m,0: expected {} values, got {}", nb_processes, values_text.size()));
+                }
+                for(ProcessId m = 0; m < nb_processes; m += 1) {
+                    estimated_a.set_0(m, parse_double(values_text[m], "a_{m,0}"));
+                }
+            }
+            for(ProcessId l = 0; l < nb_processes; l += 1) {
+                for(FunctionBaseId k = 0; k < base_size; k += 1) {
+                    auto values_text = split('\t', get_next_non_comment_line());
+                    if(values_text.size() != nb_processes) {
+                        throw std::runtime_error(fmt::format(
+                            "a_{{m,l={},k={}}}: expected {} values, got {}", l, k, nb_processes, values_text.size()));
+                    }
+                    for(ProcessId m = 0; m < nb_processes; m += 1) {
+                        estimated_a.set_lk(m, l, k, parse_double(values_text[m], "a_{m,l,k}"));
+                    }
+                }
+            }
+        } catch(const std::runtime_error & e) {
+            throw std::runtime_error(
+                fmt::format("Parsing estimated a at line {}: {}", reader.current_line_number() + 1, e.what()));
+        }
+        const auto end = instant();
+        fmt::print(stderr, "Estimated a loaded from '{}': time = {}\n", filename, duration_string(end - start));
+        return estimated_a;
+    } catch(const std::runtime_error & e) {
+        throw std::runtime_error(fmt::format("Reading estimated a from {}: {}", filename, e.what()));
+    }
+}
+
 /******************************************************************************
  * Program entry point.
  */
@@ -16,6 +67,7 @@ int main(int argc, char * argv[]) {
 
     std::unique_ptr<HistogramBase> base;
     std::vector<ProcessFile> process_files;
+    string_view estimated_a_filename;
     bool dump_region_info_option = false;
 
     parser.flag({"h", "help"}, "Display this help", [&]() {
@@ -47,6 +99,11 @@ int main(int argc, char * argv[]) {
             process_files.emplace_back(ProcessFile{filename, ProcessDirection::Backward});
         });
 
+    parser.positional(
+        "estimated_a_filename", "Filename containing estimated a (inferrence output format)", [&](string_view value) {
+            estimated_a_filename = value;
+        });
+
     parser.flag(
         {"dump-region-info"}, "Stop after parsing and print region/process point counts", [&dump_region_info_option]() {
             dump_region_info_option = true;
@@ -69,9 +126,9 @@ int main(int argc, char * argv[]) {
         // Read input files
         const auto data_points = read_process_files(process_files);
         const auto points = extract_point_lists(data_points);
+        auto estimated_a = read_estimated_a_from(estimated_a_filename, points.nb_processes(), base->base_size);
 
-        // TODO parsing from file
-        auto estimated_a = Matrix_M_MK1(points.nb_processes(), base->base_size);
+        fmt::print("{}\n", estimated_a.inner);
 
     } catch(const CommandLineParser::Exception & exc) {
         fmt::print(stderr, "Error: {}. Use --help for a list of options.\n", exc.what());
